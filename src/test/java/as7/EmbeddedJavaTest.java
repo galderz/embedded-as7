70fcaffcb181bffe3eb44551b057c58973f1ad2b
Galder Zamarreño <galder@zamarreno.com>
2012-11-13 11:43:17 +0100

    Embedded AS7 finally starts up!

--- src/test/java/as7/EmbeddedJavaTest.java
+++ src/test/java/as7/EmbeddedJavaTest.java
@@ -1,5 +1,15 @@
 package as7;
 
+import org.jboss.arquillian.container.spi.Container;
+import org.jboss.arquillian.container.spi.ContainerRegistry;
+import org.jboss.arquillian.container.spi.client.container.DeployableContainer;
+import org.jboss.arquillian.container.spi.client.deployment.TargetDescription;
+import org.jboss.arquillian.container.spi.event.SetupContainer;
+import org.jboss.arquillian.container.spi.event.StartContainer;
+import org.jboss.arquillian.core.impl.loadable.LoadableExtensionLoader;
+import org.jboss.arquillian.core.spi.Manager;
+import org.jboss.arquillian.core.spi.ManagerBuilder;
+import org.jboss.as.arquillian.container.CommonContainerExtension;
 import org.jboss.as.arquillian.container.embedded.EmbeddedContainerConfiguration;
 import org.jboss.as.arquillian.container.embedded.EmbeddedDeployableContainer;
 import org.junit.Test;
@@ -25,19 +35,39 @@ public class EmbeddedJavaTest {
       String home = System.getProperty("user.home");
       String jbossHome = tmpDir + "/jboss-as-7.x.incremental.546";
       File jbossHomeDir = new File(jbossHome);
+      File jbossCfg = new File(jbossHome + "/standalone/configuration/standalone.xml");
 
-      if (!jbossHomeDir.exists())
+      if (!jbossCfg.exists())
          unzip(home + "/.m2/repository/" +
-            "org/jboss/as/jboss-as-dist/7.x.incremental.546/" +
-            "jboss-as-dist-7.x.incremental.546.zip",
-            tmpDir);
+               "org/jboss/as/jboss-as-dist/7.x.incremental.546/" +
+               "jboss-as-dist-7.x.incremental.546.zip",
+               tmpDir);
 
       // TODO: ALR, why not use 'jboss.home.dir' ? ServerEnvironment.HOME_DIR
       System.setProperty("jboss.home", jbossHomeDir.getAbsolutePath());
 
-      EmbeddedDeployableContainer container = new EmbeddedDeployableContainer();
-      container.setup(new EmbeddedContainerConfiguration());
-      container.start();
+      // 1. Start arquillian manager
+      Manager manager = ManagerBuilder.from()
+            .extension(LoadableExtensionLoader.class).create();
+      manager.start();
+      // 2. Resolve container registry
+      ContainerRegistry registry = manager.resolve(ContainerRegistry.class);
+      if (registry == null) {
+         throw new IllegalStateException("No ContainerRegistry found in Context. Something is wrong with the classpath.....");
+      }
+
+      if (registry.getContainers().size() == 0) {
+         throw new IllegalStateException(
+               "No Containers in registry. You need to add the Container Adaptor dependencies to the plugin dependency section");
+      }
+      // 2. Get container
+      Container container = registry.getContainer(TargetDescription.DEFAULT);
+      System.out.println("to container: " + container.getName());
+      // 3. Fire events to setup and start
+      manager.fire(new SetupContainer(container));
+      manager.fire(new StartContainer(container));
+
+
    }
 
    public static void unzip(String zipFile, String outputFolder) throws Exception {
@@ -75,5 +105,17 @@ public class EmbeddedJavaTest {
 
       System.out.println("Done");
    }
+
+//   public static class EmbeddedContainerExtension
+//         extends CommonContainerExtension {
+//
+//      @Override
+//      public void register(final ExtensionBuilder builder) {
+//         super.register(builder);
+//         builder.service(DeployableContainer.class,
+//               EmbeddedDeployableContainer.class);
+//      }
+//
+//   }
 
 }
